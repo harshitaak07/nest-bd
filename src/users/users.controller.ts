038b6d3d290964a07f6e6c0abb3ad7e80ca53867
Harshitaa Kashyap <suzimayisbae@gmail.com>
2024-10-26 15:27:25 +0530

    feat: controller, service, dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,35 +4,48 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Query,
+  ValidationPipe,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user-dto';
+import { UpdateUserDto } from './dto/update-user-dto';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
   @Get()
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
+    return this.usersService.findAll(role);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return [];
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.findOne(id);
   }
 
   @Post()
-  createOne(@Body() user: {}) {
-    return user;
+  createOne(
+    @Body(ValidationPipe)
+    createUserDto: CreateUserDto,
+  ) {
+    return this.usersService.createOne(createUserDto);
   }
 
   @Patch(':id')
-  updateOne(@Param('id') id: string, @Body() userUpdate: {}) {
-    return userUpdate;
+  updateOne(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe)
+    updateUserDto: UpdateUserDto,
+  ) {
+    return this.usersService.updateOne(id, updateUserDto);
   }
 
   @Delete(':id')
-  deleteOne(@Param('id') id: string) {
-    return [];
+  deleteOne(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.deleteOne(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user-dto';
+import { UpdateUserDto } from './dto/update-user-dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      email: 'Sincere@april.biz',
+      role: 'INTERN',
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      email: 'Shanna@melissa.tv',
+      role: 'INTERN',
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      email: 'Nathan@yesenia.net',
+      role: 'ENGINEER',
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      email: 'Julianne.OConner@kory.org',
+      role: 'ENGINEER',
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      email: 'Lucio_Hettinger@annie.ca',
+      role: 'ADMIN',
+    },
+  ];
+
+  /*
+  1. The role parameter is optional. If a role is provided, the function filters this.users to return only those users that match the specified role. 
+  If no role is provided, it simply returns all users.
+  2. Filter: Creates a new array with all elements that pass the test implemented by the provided function.
+  3. If no elements match, it returns an empty array.
+  4. Use return this.users to return the full collection of users.
+  5. If you write return users, the JavaScript engine looks for a variable named users in the current function scope.
+  6. If no such variable exists, it will throw a ReferenceError.
+  7. Find: Returns the first element in the array that satisfies the provided testing function.
+  8. If no elements satisfy the testing function, it returns undefined.
+  9. Use return user to return a specific user (usually after using find).
+  */
+
+  findAll(role?: 'ADMIN' | 'ENGINEER' | 'INTERN') {
+    if (role) {
+      const rolesArray = this.users.filter((user) => user.role === role);
+      if (rolesArray.length === 0) {
+        throw new NotFoundException('User Role Not found');
+        return rolesArray;
+      }
+    }
+    return this.users;
+  }
+
+  findOne(id: number) {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    return user;
+  }
+
+  createOne(createUserDto: CreateUserDto) {
+    const usersByHighestId = { ...this.users }.sort((a, b) => b.id - a.id);
+    const newUser = {
+      id: usersByHighestId[0].id + 1,
+      ...createUserDto,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  updateOne(id: number, updateUserDto: UpdateUserDto) {
+    this.users = this.users.map((user) => {
+      if (user.id === id) {
+        return { ...user, ...updateUserDto };
+      }
+      return user;
+    });
+    return this.findOne(id);
+  }
+
+  deleteOne(id: number) {
+    const removedUser = this.findOne(id);
+    this.users = this.users.filter((user) => user.id === id);
+    return removedUser;
+  }
+}
